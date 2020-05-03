428f29ed31dcc78b09c62cd288985f4ac3f477a5
Piotr Kalman <piotr.kalman85@gmail.com>
2020-05-03 20:43:42 +0200

    responsive canvas autoresize

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const wheelContainer = document.querySelector('.container');
 const startWheel = document.querySelector('.btn-danger');
 const drawnElementsList = document.querySelector('.drawnElementsList');
 const countSpan = document.querySelector('.actual span');
+const canvas = document.querySelector('canvas');
 
 //other variables
 let wheelDeg = 0;
@@ -57,11 +58,12 @@ const addInputs = () => {
     names = nameInputs.querySelectorAll('.nameInput');
     amounts = nameInputs.querySelectorAll('.inputHowMany');
     namesSubmit = document.querySelector('.names-submit');
-    namesSubmit.addEventListener('click', constructWheelWithCut)
+    namesSubmit.addEventListener('click', () => constructWheelWithCut())
 }
 
 // main function to construct wheel from parts input
 const constructWheelWithCut = () => {
+    wheelNames = ['helloGuinea'];
     startWheel.removeAttribute('disabled');
     wheelContainer.style.display = 'flex';
     namesSubmit.parentElement.style.display = 'none';
@@ -80,64 +82,6 @@ const constructWheelWithCut = () => {
             )
         }
     });
-
-    //construct wheel in canvas
-    wheelEngine = () => {
-        if (wheelNames.length % 10 == 1) {
-            color = ['#f40552', '#c3edea', '#fc7e2f', '#f8f3eb', '#6886c5', '#ffacb7', '#ffe0ac', '#43d8c9', '#f4eeff', '#ffd868', '#baf1a1'];
-        } else {
-            color = ['#f40552', '#c3edea', '#fc7e2f', '#f8f3eb', '#6886c5', '#ffacb7', '#ffe0ac', '#43d8c9', '#f4eeff', '#ffd868'];
-        }
-        const slices = wheelNames.length;
-        const sliceDeg = 360 / slices;
-        let deg = 0;
-        const canvas = document.querySelector('canvas');
-        const ctx = canvas.getContext('2d');
-        const width = canvas.width;
-        const center = width / 2;
-        function deg2rad(deg) { return deg * Math.PI / 180; }
-
-        function drawSlice(deg, color) {
-            ctx.beginPath();
-            ctx.fillStyle = color;
-            ctx.moveTo(center, center);
-            ctx.arc(center, center, width / 2, deg2rad(deg), deg2rad(deg + sliceDeg));
-            ctx.lineTo(center, center);
-            ctx.fill();
-        }
-
-        function drawText(deg, text) {
-            ctx.save();
-            ctx.translate(center, center);
-            ctx.rotate(deg2rad(deg));
-            ctx.textAlign = "left";
-            ctx.fillStyle = "#003f8a";
-            ctx.font = 'bold 30px sans-serif';
-            ctx.strokeStyle = "#FF0000"
-            ctx.fillText(text, 120, 10);
-            ctx.restore();
-        }
-
-        for (var i = 0; i < slices; i++) {
-            drawSlice(deg, color[(i % color.length)]);
-            drawText(deg + sliceDeg / 2, wheelNames[i]);
-            deg += sliceDeg;
-        }
-
-        showDrawnElementsList = () => {
-            const listElements = drawnElementsList.querySelectorAll('li');
-            if (listElements.length > 0) {
-                listElements.forEach(element => element.remove());
-            }
-            for (i = 0; i < drawnElements.length; i++) {
-                const li = document.createElement('li');
-                li.innerHTML = drawnElements[i];
-                drawnElementsList.appendChild(li);
-            }
-        }
-
-        showDrawnElementsList()
-    }
 
     wheelEngine();
 
@@ -182,6 +126,101 @@ const constructWheelWithCut = () => {
     }
 }
 
+//function to fit canvas size
+const canvasSize = () => {
+    if (window.innerWidth < 450) {
+        canvas.setAttribute('width', '230');
+        canvas.setAttribute('height', '230');
+    }
+
+    else if (window.innerWidth < 850) {
+        canvas.setAttribute('width', '400');
+        canvas.setAttribute('height', '400');
+    }
+    else {
+        canvas.setAttribute('width', '600');
+        canvas.setAttribute('height', '600');
+    }
+    if (piecesSubmit.disabled) {
+        if (piecesSubmit.disabled) {
+            wheelEngine();
+        }
+    }
+}
+
+canvasSize();
+
+//construct wheel in canvas
+const wheelEngine = () => {
+    // console.log('hello Guinea pig')
+    if (wheelNames.length % 10 == 1) {
+        color = ['#f40552', '#c3edea', '#fc7e2f', '#f8f3eb', '#6886c5', '#ffacb7', '#ffe0ac', '#43d8c9', '#f4eeff', '#ffd868', '#baf1a1'];
+    } else {
+        color = ['#f40552', '#c3edea', '#fc7e2f', '#f8f3eb', '#6886c5', '#ffacb7', '#ffe0ac', '#43d8c9', '#f4eeff', '#ffd868'];
+    }
+    const slices = wheelNames.length;
+    const sliceDeg = 360 / slices;
+    let deg = 0;
+
+    const ctx = canvas.getContext('2d');
+    const width = canvas.width;
+    const center = width / 2;
+    function deg2rad(deg) { return deg * Math.PI / 180; }
+
+    function drawSlice(deg, color) {
+        ctx.beginPath();
+        ctx.fillStyle = color;
+        ctx.moveTo(center, center);
+        ctx.arc(center, center, width / 2, deg2rad(deg), deg2rad(deg + sliceDeg));
+        ctx.lineTo(center, center);
+        ctx.fill();
+    }
+
+    if (window.innerWidth > '850px') {
+
+    }
+    function drawText(deg, text) {
+        ctx.save();
+        ctx.translate(center, center);
+        ctx.rotate(deg2rad(deg));
+        ctx.textAlign = "left";
+        ctx.fillStyle = "#003f8a";
+        if (window.innerWidth < 500) {
+            ctx.font = 'bold 15px lato';
+            ctx.fillText(text, 40, 4);
+        } else if (window.innerWidth < 850 || wheelNames > 30) {
+            ctx.font = 'bold 20px lato';
+            ctx.fillText(text, 100, 10);
+        } else {
+            ctx.font = 'bold 30px lato';
+            ctx.fillText(text, 100, 10);
+        }
+        ctx.restore();
+    }
+
+    for (var i = 0; i < slices; i++) {
+        drawSlice(deg, color[(i % color.length)]);
+        drawText(deg + sliceDeg / 2, wheelNames[i]);
+        deg += sliceDeg;
+    }
+
+    showDrawnElementsList = () => {
+        const listElements = drawnElementsList.querySelectorAll('li');
+        if (listElements.length > 0) {
+            listElements.forEach(element => element.remove());
+        }
+        for (i = 0; i < drawnElements.length; i++) {
+            const li = document.createElement('li');
+            li.innerHTML = drawnElements[i];
+            drawnElementsList.appendChild(li);
+        }
+    }
+
+    showDrawnElementsList()
+}
+
+//set canvas size automatically
+window.addEventListener("resize", canvasSize);
+
 piecesSubmit.addEventListener('click', addInputs);
 newWheelBtn.addEventListener('click', () => location.reload());
-
