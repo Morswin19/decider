fe916cf3cb1c1b870a98650dc397bdcc1fdc015e
Morswin19 <piotr.kalman85@gmail.com>
2020-08-07 20:09:19 +0200

    simple functionality add

--- app.js
+++ app.js
@@ -1,154 +1,8 @@
-//DOM variables
-const piecesInput = document.querySelector('.piecesForm input');
-const piecesSubmit = document.querySelector('.piecesForm button');
-const piecesDiv = document.querySelector('.piecesForm div');
-const newWheelBtn = document.querySelector('.new-wheel');
-const nameInputs = document.querySelector('.nameInputs');
-const radioNot = document.querySelector('#notCut');
-const radioYes = document.querySelector('#elementCut');
-let names = nameInputs.querySelectorAll('input')
-let amounts = nameInputs.querySelectorAll('inputHowMany');
-let namesSubmit = document.querySelector('.names-submit');
-const wheelContainer = document.querySelector('.container');
-const startWheel = document.querySelector('.btn-danger');
-const drawnElementsList = document.querySelector('#play');
-const countSpan = document.querySelector('.actual span');
+const inputs = document.querySelector(".option")
+
+let wheelNames = ["Lusia", "Jaga", "Diesel", "Gadolina"];
 const canvas = document.querySelector('canvas');
-
-//other variables
-let wheelDeg = 0;
-let wheelNames = [];
-let randomNumbers = [];
-let randomNames = [];
-let color = [];
-let drawnElements = [];
-let startCount = 0;
-
-//add inputs after submit wheel type and parts amount
-const addInputs = () => {
-    piecesSubmit.setAttribute('disabled', true);
-    newWheelBtn.style.display = 'inline-block';
-    const pieces = piecesInput.value;
-    //elements for every part
-    for (i = 1; i <= pieces; i++) {
-        const div = document.createElement('div');
-        div.className = 'nameDiv';
-        const label = document.createElement('label');
-        label.appendChild(document.createTextNode(`${i}. element:`));
-        const input = document.createElement('input');
-        input.className = `nameInput input${i}`;
-        const label2 = document.createElement('label');
-        label2.appendChild(document.createTextNode(`how many:`));
-        const input2 = document.createElement('input');
-        input2.className = `inputHowMany inputHowMany${i}`;
-        input2.setAttribute('value', '1')
-        input2.setAttribute('type', 'number')
-        input2.setAttribute('min', '0');
-        nameInputs.appendChild(div)
-        div.appendChild(label);
-        div.appendChild(input);
-        div.appendChild(label2);
-        div.appendChild(input2);
-    }
-    const button = document.createElement('button');
-    button.className = 'btn btn-info names-submit';
-    button.appendChild(document.createTextNode('Submit'));
-    nameInputs.parentNode.appendChild(button);
-    button.style.marginBottom = '10px';
-    names = nameInputs.querySelectorAll('.nameInput');
-    amounts = nameInputs.querySelectorAll('.inputHowMany');
-    namesSubmit = document.querySelector('.names-submit');
-    namesSubmit.addEventListener('click', () => constructWheelWithCut())
-}
-
-// main function to construct wheel from parts input
-const constructWheelWithCut = () => {
-    wheelNames = ['helloGuinea'];
-    startWheel.removeAttribute('disabled');
-    wheelContainer.style.display = 'flex';
-    namesSubmit.parentElement.style.display = 'none';
-    piecesDiv.style.display = 'none';
-    piecesSubmit.style.display = 'none';
-    namesSubmit.disabled = 'true';
-    names.forEach((name, index) => {
-        for (i = 1; i <= amounts[index].value; i++) {
-            const number = Math.floor(Math.random() * 1000);
-            randomNames.push(number + '&8&' + name.value)
-            randomNames = randomNames.sort();
-            wheelNames = randomNames.map(name => {
-                let index = name.indexOf('&8&')
-                return name.slice(index + 3);
-            }
-            )
-        }
-    });
-
-    wheelEngine();
-
-    handleClick = () => {
-        const deg = Math.floor(Math.random() * 3000);
-        if (deg < 360) {
-            return this.handleClick();
-        }
-        wheelDeg = wheelDeg + deg
-        const canvas = document.querySelector('canvas')
-        canvas.style.transform = `rotate(${wheelDeg}deg)`;
-        startWheel.disabled = 'true';
-        let degLeft = wheelDeg % 360;
-        let oneSlice = 360 / wheelNames.length;
-        const drawnElement = wheelNames.filter((name, index) => (index * oneSlice < (360 - degLeft)) && ((index + 1) * oneSlice >= (360 - degLeft)))
-        if (drawnElement[0] !== undefined) {
-            drawnElements.unshift(drawnElement[0]);
-        }
-        if (radioYes.checked == true) {
-            setTimeout(() => elementCut(), 7000);
-        } else {
-            setTimeout(() => {
-                (startWheel.removeAttribute('disabled'));
-                wheelEngine();
-                startCount++;
-                countSpan.innerHTML = `(${startCount})`;
-            }, 5000)
-        }
-    }
-
-    //function to cutting elements in remove element version
-    elementCut = () => {
-        startWheel.removeAttribute('disabled')
-        let degLeft = wheelDeg % 360;
-        let oneSlice = 360 / wheelNames.length;
-        const nameToCut = wheelNames.filter((name, index) => (index * oneSlice < (360 - degLeft)) && ((index + 1) * oneSlice >= (360 - degLeft)))
-        const index = wheelNames.indexOf(nameToCut[0]);
-        wheelNames.splice(index, 1);
-        startCount++;
-        countSpan.innerHTML = `(${startCount})`;
-        wheelEngine();
-    }
-}
-
-//function to fit canvas size
-const canvasSize = () => {
-    if (window.innerWidth < 450) {
-        canvas.setAttribute('width', '230');
-        canvas.setAttribute('height', '230');
-    }
-
-    else if (window.innerWidth < 850) {
-        canvas.setAttribute('width', '400');
-        canvas.setAttribute('height', '400');
-    }
-    else {
-        canvas.setAttribute('width', '600');
-        canvas.setAttribute('height', '600');
-    }
-    if (piecesSubmit.disabled) {
-        if (piecesSubmit.disabled) {
-            wheelEngine();
-        }
-    }
-}
-
-canvasSize();
+const playBtn = document.querySelector('#play')
 
 //construct wheel in canvas
 const wheelEngine = () => {
@@ -204,23 +58,48 @@ const wheelEngine = () => {
         deg += sliceDeg;
     }
 
-    showDrawnElementsList = () => {
-        const listElements = drawnElementsList.querySelectorAll('li');
-        if (listElements.length > 0) {
-            listElements.forEach(element => element.remove());
-        }
-        for (i = 0; i < drawnElements.length; i++) {
-            const li = document.createElement('li');
-            li.innerHTML = drawnElements[i];
-            drawnElementsList.appendChild(li);
-        }
-    }
+    // showDrawnElementsList = () => {
+    //     const listElements = drawnElementsList.querySelectorAll('li');
+    //     if (listElements.length > 0) {
+    //         listElements.forEach(element => element.remove());
+    //     }
+    //     for (i = 0; i < drawnElements.length; i++) {
+    //         const li = document.createElement('li');
+    //         li.innerHTML = drawnElements[i];
+    //         drawnElementsList.appendChild(li);
+    //     }
+    // }
 
-    showDrawnElementsList()
+    // showDrawnElementsList()
 }
 
-//set canvas size automatically
-window.addEventListener("resize", canvasSize);
+wheelEngine()
 
-piecesSubmit.addEventListener('click', addInputs);
-newWheelBtn.addEventListener('click', () => location.reload());
+let wheelDeg = 0
+
+const handleClick = () => {
+    const deg = Math.floor(Math.random() * 3000);
+    if (deg < 360) {
+        return this.handleClick();
+    }
+    wheelDeg = wheelDeg + deg
+    const canvas = document.querySelector('canvas')
+    canvas.style.transform = `rotate(${wheelDeg}deg)`;
+    // startWheel.disabled = 'true';
+    let degLeft = wheelDeg % 360;
+    let oneSlice = 360 / wheelNames.length;
+    const drawnElement = wheelNames.filter((name, index) => (index * oneSlice < (360 - degLeft)) && ((index + 1) * oneSlice >= (360 - degLeft)))
+    if (drawnElement[0] !== undefined) {
+        drawnElements.unshift(drawnElement[0]);
+    }
+    if (radioYes.checked == true) {
+        setTimeout(() => elementCut(), 7000);
+    } else {
+        setTimeout(() => {
+            (startWheel.removeAttribute('disabled'));
+            wheelEngine();
+            startCount++;
+            countSpan.innerHTML = `(${startCount})`;
+        }, 5000)
+    }
+}
