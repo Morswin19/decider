2c1f48c0f220affb65e17c6ca1dd2e93c76a255c
Morswin19 <piotr.kalman85@gmail.com>
2020-08-09 10:32:12 +0200

    more rwd changes

--- app.js
+++ app.js
@@ -184,7 +184,7 @@ wheelNamesChanged()
 
 // function to fit canvas size
 const canvasSize = () => {
-    if (document.body.clientWidth < 500) {
+    if (window.innerWidth < 500) {
         canvas.setAttribute('width', '250');
         canvas.setAttribute('height', '250');
     }
